a90fb443bc4111acb332f2673c3ebc1943ae2286
avidsaputro <avidsaputro@students.amikom.ac.id>
2021-03-31 20:22:14 +0700

    inisialisasi projek

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contohoverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            numbers number = new numbers();
+            Console.WriteLine("Minimum1: {0}", number.findminimum(3,1));
+            Console.WriteLine("Minimum2: {0}", number.findminimum(3, 8, 4));
+            Console.WriteLine();
+            Console.WriteLine("Minimum1: {0}", number.findmaximum(3, 1));
+            Console.WriteLine("Minimum2: {0}", number.findmaximum(3, 8, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ numbers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contohoverloading
+{
+    class numbers
+    {
+        public int findminimum (int numb1 , int numb2)
+        {
+            int min;
+            min = numb2;
+            if( min > numb1)
+            {
+                min = numb1;
+            }
+            return min;
+        }
+        public float findminimum(int numb1, int numb2, float numb3)
+        {
+            float min;
+            min = numb2;
+            if (min > numb1)
+            {
+                min = numb1;
+            }
+            if(min > numb3)
+            {
+                min = numb3;
+            }
+            return min;
+        }
+        public int findmaximum(int numb1 , int numb2)
+        {
+            int max;
+            max = numb1;
+            if(numb2 > max)
+            {
+                max = numb2;
+            }
+            return max;
+        }
+        public float findmaximum(int numb1, int numb2, float numb3)
+        {
+            float max;
+            max = numb1;
+            if (numb2 > max)
+            {
+                max = numb2;
+            }
+            if (numb3 > max)
+            {
+                max = numb3;
+            }
+            return max;
+        }
+    }
+}
